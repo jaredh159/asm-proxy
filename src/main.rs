dd8a7c6c8a0fbf5acc5a76fdd406845c17fc5a51
Jared Henderson <jaredh159@users.noreply.github.com>
2024-07-03 16:57:23 -0400

    receive exec command from http req

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use std::{env, fs};
 #[derive(Deserialize, Debug)]
 struct JobInput {
   asm: String,
+  exec: String,
 }
 
 #[derive(Serialize, Debug)]
@@ -39,24 +40,22 @@ impl IntoResponse for BadRequest {
 /// Execute arbritary assembly code, what could possibly go wrong?
 #[axum_macros::debug_handler]
 async fn handle_job(
-  AuthBearer(token): AuthBearer,
+  AuthBearer(input_token): AuthBearer,
   Json(input): Json<JobInput>,
 ) -> Result<Json<JobOutput>, BadRequest> {
-  let check_token = env::var("ACCESS_TOKEN").unwrap();
-  if token != check_token {
+  let Some(access_token) = env::var("ACCESS_TOKEN").ok() else {
+    return Err(BadRequest);
+  };
+  if input_token != access_token {
     return Err(BadRequest);
   }
 
-  let fileext = env::var("ASM_FILE_EXT").unwrap();
-  let assemble_link_cmd = env::var("ASSEMBLE_LINK_EXEC_CMD").unwrap();
-  let filename = format!("program.{}", fileext);
-  clean_files(&filename);
-
-  fs::write(&filename, input.asm).unwrap();
+  clean_files();
+  fs::write("program.s", input.asm).unwrap();
 
   let output = Command::new("sh")
     .arg("-c")
-    .arg(assemble_link_cmd)
+    .arg(input.exec)
     .stdout(Stdio::piped())
     .stderr(Stdio::piped())
     .output()
@@ -65,7 +64,7 @@ async fn handle_job(
   let stdout = String::from_utf8(output.stdout).unwrap();
   let stderr = String::from_utf8(output.stderr).unwrap();
   let exit_code = output.status.code().unwrap();
-  clean_files(&filename);
+  clean_files();
 
   Ok(Json(JobOutput { exit_code, stdout, stderr }))
 }
@@ -74,12 +73,8 @@ async fn handle_job(
 async fn main() {
   dotenv().ok();
 
-  // build our application with a single route
-  let app = Router::new()
-    .route("/asm", post(handle_job))
-    .route("/", get(|| async { "Hello, World!" }));
+  let app = Router::new().route("/asm", post(handle_job));
 
-  // run our app with hyper, listening globally on port 3000
   let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
     .await
     .unwrap();
@@ -88,10 +83,8 @@ async fn main() {
   axum::serve(listener, app).await.unwrap();
 }
 
-fn clean_files(filename: &str) {
+fn clean_files() {
   _ = fs::remove_file("obj.o");
   _ = fs::remove_file("a.out");
-  _ = fs::remove_file("program.asm");
   _ = fs::remove_file("program.s");
-  _ = fs::remove_file(filename);
 }
